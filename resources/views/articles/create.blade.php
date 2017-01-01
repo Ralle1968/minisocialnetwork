5e4d65d5f412fa8c409182859956244095db263c
Ralle1968 <graef.ralf@gmail.com>
2017-01-01 15:33:55 +0100

    changes for crud: store

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,6 +10,9 @@
 				<div class="panel-body">
 					<form action="/articles" method="POST">
 						{{ csrf_field() }}
+
+						<input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+
 						<div class="form-group">
 							<label for="content">Content</label>
 							<textarea name="content" class="form-control"></textarea>
